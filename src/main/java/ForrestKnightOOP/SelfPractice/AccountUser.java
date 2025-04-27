f48cf88138485d909a2cd39403068cb0e3f21fd4
Theresaa-K <kennytheresa7@gmail.com>
2025-04-27 10:15:17 -0500

    Updated ifStatements: User prompts continue questions after first prompt

--- src/main/java/ForrestKnightOOP/SelfPractice/AccountUser.java
+++ src/main/java/ForrestKnightOOP/SelfPractice/AccountUser.java
@@ -11,6 +11,7 @@ public class AccountUser extends BankAccount {
         String response1;
         String response2;
         String response3;
+        String response4;
         double amount1;
         double amount2;
 
@@ -19,12 +20,15 @@ public class AccountUser extends BankAccount {
         if (response1.equalsIgnoreCase("yes")) {
             //Enter the balance logic
             BankAccount.viewBalance(BankAccount.getBalance()); // Call the viewBalance method
-        } else {
-            System.out.println("Okay, would you like to send some money? (yes/no)");
+            System.out.println("\nOkay, would you like to send some money? (yes/no)");
             response2 = scanner.nextLine(); // If the user does not want to see their balance
             if (response2.equalsIgnoreCase("yes")) {
                 System.out.println("How much money would you like to send?");
                 amount1 = scanner.nextDouble(); // Get the amount to send
+                System.out.println("Who you would like to send the money to: Please enter the name and phone number of the person you are sending money to.");
+                response4 = scanner.next(); // Get the name of the person to send money to
+
+                System.out.println("You have selected: "+ response4 );
                 BankAccount.sendMoney(amount1); // Call the sendMoney method
             } else
             { System.out.println("Okay, would you like to take some money out? (yes/no)");
@@ -33,11 +37,12 @@ public class AccountUser extends BankAccount {
                     System.out.println("How much money would you like to take out?");
                     amount2 = scanner.nextDouble(); // Get the amount to take out
                     BankAccount.takeOutMoney(amount2); // Call the takeOutMoney method
-                }else{
-                    System.out.println("You chose to: not see your balance, takeout or send money. Have a good day.");
+        } else  {
+                    System.out.println("Okay, have a nice day!");
                 }
-
             }
+        } else {
+            System.out.println("Okay, have a nice day!"); // If the user does not want to see their balance
         }
     }
 }
